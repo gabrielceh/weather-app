434aacf1b16c56519c484538d2c83e319607e45e
Gabriel Cervantes <76570271+gabrielceh@users.noreply.github.com>
2023-07-16 18:00:12 -0500

    message fav empty

--- src/components/FavoritesMenu.jsx
+++ src/components/FavoritesMenu.jsx
@@ -14,28 +14,34 @@ function FavoritesMenu({ isOpen, onclose, anchorEl }) {
 	return (
 		<Menu open={isOpen} onClose={onclose} anchorEl={anchorEl}>
 			<MenuList sx={{ minWidth: '250px' }}>
-				{favoritesState.favorites.map((fav) => (
-					<MenuItem
-						key={fav.id}
-						component={RouterLink}
-						to={`${routesName.favorites}?lat=${fav.lat}&lon=${fav.lon}`}
-						onClick={onclose}
-						sx={{
-							textTransform: 'capitalize',
-							width: '100%',
-							display: 'flex',
-							justifyContent: 'space-between',
-							alignItems: 'center',
-						}}>
-						<Typography component='span'>
-							{fav.name}, {fav.country}
-						</Typography>
-						<Typography
-							component='span'
-							className={`fi fi-${fav.country.toLowerCase()}`}
-							sx={{ position: 'relative', zIndex: 0 }}></Typography>
+				{favoritesState.favorites.length <= 0 && (
+					<MenuItem>
+						<Typography>Nothing here! 😢</Typography>
 					</MenuItem>
-				))}
+				)}
+				{favoritesState.favorites.length > 0 &&
+					favoritesState.favorites.map((fav) => (
+						<MenuItem
+							key={fav.id}
+							component={RouterLink}
+							to={`${routesName.favorites}?lat=${fav.lat}&lon=${fav.lon}`}
+							onClick={onclose}
+							sx={{
+								textTransform: 'capitalize',
+								width: '100%',
+								display: 'flex',
+								justifyContent: 'space-between',
+								alignItems: 'center',
+							}}>
+							<Typography component='span'>
+								{fav.name}, {fav.country}
+							</Typography>
+							<Typography
+								component='span'
+								className={`fi fi-${fav.country.toLowerCase()}`}
+								sx={{ position: 'relative', zIndex: 0 }}></Typography>
+						</MenuItem>
+					))}
 			</MenuList>
 		</Menu>
 	)
